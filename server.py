b002ca3901e0c7ab8fad07a4ac687a263a10df3f
dean0coin <69442911+dean0coin@users.noreply.github.com>
2021-01-08 23:32:09 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '162.216.17.32'
+PORT = 89322
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+client, address = server.accept()
+
+print(f"Connected to {address}.")
+while True:
+    cmd_input = input("COMMAND: ")
+    client.send(cmd_input.encode('utf-8'))
+    print(client.recv(1024).decode('utf-8'))
